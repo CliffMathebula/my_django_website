0334b0bc3eae14c5c28065189272ff9d63f09fa5
cliff-mathebula <47654155@mylife.unisa.ac.za>
2020-04-18 11:53:50 +0200

    Refactor : modified the model database fields to new fields

--- post/models.py
+++ post/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
+
 class Post(models.Model):
-    text = models.TextField()
+    name = models.TextField()
+    surname = models.TextField()
+    id_number = models.IntegerField()
+    email_address = models.EmailField()
+    username = models.CharField(max_length=50)
+    password = models.CharField(max_length = 50)
+    user_id = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
-    
-        return self.text
+        return self.name
