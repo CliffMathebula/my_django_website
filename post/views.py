6bb02eca8e9dae57188a796b489eec3da40f2410
cliff-mathebula <47654155@mylife.unisa.ac.za>
2020-04-18 15:35:13 +0200

    added the Class UserCreate

--- post/views.py
+++ post/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
 from .models import Post
 
 class HomePageView(ListView):
@@ -7,3 +7,8 @@ class HomePageView(ListView):
 
     template_name = 'home.html'
     context_object_name = 'all_posts_list' # new
+
+class UserCreateView(CreateView): # new
+    model = Post
+    template_name = 'post_new.html'
+    fields = '__all__'
