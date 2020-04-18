fd6a768c1b3834d616865640ffb8d84126e429e4
cliff-mathebula <47654155@mylife.unisa.ac.za>
2020-04-18 15:35:54 +0200

    commit for git push

--- post/urls.py
+++ post/urls.py
@@ -1,7 +1,12 @@
 # post/urls.py
 from django.urls import path
 from .views import HomePageView
+from .views import UserCreateView
+from .views import UserDetailView
 
 urlpatterns = [
 path('', HomePageView.as_view(), name='home'),
+path('post/new/', UserCreateView.as_view(), name='post_new'), # new
+path('post/<int:pk>/', UserDetailView.as_view(), name='post_detail'),
 ]
+
