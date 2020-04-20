1ce2b614e90059796173567f23e801559aede59b
cliff-mathebula <47654155@mylife.unisa.ac.za>
2020-04-20 14:45:15 +0200

    added the signup page on accounts directory

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,9 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
+
     'post.apps.PostConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- mysite/urls.py
+++ mysite/urls.py
@@ -3,6 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('post.urls')),
-    path('accounts/', include('django.contrib.auth.urls')), # new
+    path('accounts/', include('django.contrib.auth.urls')),  # new
+    path('accounts/', include('accounts.urls')),  # new
     path('admin/', admin.site.urls),
 ]
